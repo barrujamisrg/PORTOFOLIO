06ea9b4242dab8b98885a689116d9cb8ce589ec2
mac <mac@macs-MacBook-Pro.local>
2025-08-27 22:19:19 +0700

    Fix: Build success by using dynamic import for Skills component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-// src/app/page.js
-
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -8,7 +6,7 @@ import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
-import ProjectsWrapper from "./components/homepage/ProjectsWrapper";
+import Skills from "./components/homepage/skills";
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -32,8 +30,8 @@ export default async function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
-      <SkillsWrapper /> {/* <-- Gunakan wrapper di sini */}
-      <Projects /> 
+      <Skills />
+      <Projects />
       <Education />
       <Blog blogs={blogs} />
       <ContactSection />
